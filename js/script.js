ac87166afbac1df918d50a3c6fac45999463143e
DeviceElectronic <sdasagaston@gmail.com>
2024-08-28 07:14:51 -0500

    Update from Main2

--- js/script.js
+++ js/script.js
@@ -43,3 +43,18 @@ function filterGallery() {
 
 // Initially show all images if no filters are applied
 filterGallery();
+
+
+const linkItems = document.querySelectorAll(".link-item");
+
+linkItems.forEach((linkItem, index) => {
+    linkItem.addEventListener("click", () => {
+        document.querySelector(".active").classList.remove("active");
+        linkItem.classList.add("active");
+
+        const indicator = document.querySelector(".indicator");
+
+        // Move the indicator based on the index of the clicked link item
+        indicator.style.left = `${index * 95 + 48}px`;  // Adjust positioning accordingly
+    });
+});
